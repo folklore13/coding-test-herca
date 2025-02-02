2c6b65e7deaa189f5c14b5437b21573dac3de4af
Ario <ario.hitmeup@gmail.com>
2025-02-02 23:00:14 +0700

    Fixed bugs for Perhitungan Logic

--- app/Http/Controllers/PerhitunganController.php
+++ app/Http/Controllers/PerhitunganController.php
@@ -15,27 +15,28 @@ class PerhitunganController extends Controller
     {
         $data = DB::table('penjualans')
             ->join('marketings', 'penjualans.marketing_id', '=', 'marketings.id')
-            ->select('marketings.name', 'penjualans.date', 'penjualans.grand_total')
+            ->groupByRaw("strftime('%m', penjualans.date), marketings.name")
+            ->selectRaw("marketings.name as marketing, penjualans.date as bulan, sum(penjualans.grand_total) as omzet")
             ->get();
         
         $komisi_nominal = 0;
 
         foreach($data as $key => $value){
             $komisi = 0;
-            if($value->grand_total >= 0 && $value->grand_total < 100000000){
+            if($value->omzet >= 0 && $value->omzet < 100000000){
                 $komisi = 0;
-            } else if ($value->grand_total >= 100000000 && $value->grand_total < 200000000){
+            } else if ($value->omzet >= 100000000 && $value->omzet < 200000000){
                 $komisi = 0.025;
-            } else if ($value->grand_total >= 200000000 && $value->grand_total < 500000000){
+            } else if ($value->omzet >= 200000000 && $value->omzet < 500000000){
                 $komisi = 0.05;
-            } else if ($value->grand_total >= 500000000){
+            } else if ($value->omzet >= 500000000){
                 $komisi = 0.1;
             }
 
             Carbon::setLocale('id');
-            $value->date = Carbon::parse($value->date)->translatedFormat('F');
+            $value->bulan = Carbon::parse($value->bulan)->translatedFormat('F');
 
-            $komisi_nominal = $value->grand_total + ($value->grand_total * $komisi);
+            $komisi_nominal = $value->omzet + ($value->omzet * $komisi);
             $value->komisi = $komisi * 100 . '%';
             $value->komisi_nominal = (int) $komisi_nominal;
         }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\PerhitunganController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/perhitungan', PerhitunganController::class);
